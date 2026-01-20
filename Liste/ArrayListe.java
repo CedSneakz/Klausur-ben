a318051ff6655c6e203a0047be64015b401da659
Cedric Schirmann <cedricmarsberg@gmail.com>
2026-01-20 13:26:03 +0100

    Erste Klausuraufgabe hinzugefügt

---
+++ Liste/ArrayListe.java
@@ -0,0 +1,25 @@
+
+
+public class ArrayListe {
+
+
+public static void main(String[] args) {
+double[] noten = new double[5];
+double Durchschnitt = 0;
+        for(int i=0; i<noten.length;i++){
+            noten[i] = i + Math.random();
+            System.out.println(noten[i]);
+
+        }
+        for(int i=0; i<noten.length;i++){
+            Durchschnitt += noten[i];
+            System.out.println(Durchschnitt);
+
+        }
+        System.out.println(Durchschnitt);
+
+
+}
+    
+
+}
